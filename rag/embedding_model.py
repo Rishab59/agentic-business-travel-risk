8b66a2cea66a4eb95ba40f60954e8bb2b254f41a
Rishab Harinarayanan <2000156675@hexaware.com>
2025-12-15 15:53:50 +0530

    Embedding wrapper created

---
+++ rag/embedding_model.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer
+
+class EmbeddingModel:
+
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
+
+        self.model = SentenceTransformer(model_name)
+
+    def embed_text(self, text: str):
+
+        return self.model.encode(text)
